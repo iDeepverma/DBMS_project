fcf6f661c431a66fb1315e0b81f63c66fe0743d2
Deadshot-2002 <shivam.kulhari.cse20@itbhu.ac.in>
2022-10-28 20:13:49 +0530

    sql query corrected

--- src/main/java/com/shop/demo/dao/CustomerOrderDAO.java
+++ src/main/java/com/shop/demo/dao/CustomerOrderDAO.java
@@ -26,5 +26,5 @@ public interface CustomerOrderDAO {
     List<CustomerOrder> getCustomerOrderBetweenDates(LocalDateTime startingDate, LocalDateTime endingDate);
     //SELECT * FROM CustomerOrder WHERE orderDate BETWEEN startingDate AND endingDate;
     int getProfitPerOrder(CustomerOrder customerOrder);
-    //SELECT profit as SUM(quantity*(CustomerOrderItem.sellingPrice-Product.costPrice)) FROM Product,CustomerOrderItem WHERE CustomerOrderItem.orderID=customerOrder.orderID AND Product.productID = CustomerOrderItem.productID;
+    //SELECT SUM((quantity*(CustomerOrderItem.sellingPrice-Product.costPrice))) as profit FROM Product,CustomerOrderItem WHERE CustomerOrderItem.orderID=customerOrder.orderID AND Product.productID = CustomerOrderItem.productID;
 }
