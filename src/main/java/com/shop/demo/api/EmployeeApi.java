977d8b39f8c43168907c4095b3918ae661825a92
Deadshot-2002 <shivam.kulhari.cse20@itbhu.ac.in>
2022-10-29 13:41:37 +0530

    Revert "Revert "Fixed Employee service and tested""
    This reverts commit d518ed7a923c05e0aa3360097d14e7d2139a754d.

--- src/main/java/com/shop/demo/api/EmployeeApi.java
+++ src/main/java/com/shop/demo/api/EmployeeApi.java
@@ -3,11 +3,10 @@ package com.shop.demo.api;
 import com.shop.demo.model.Employee;
 import com.shop.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 
 @RestController
@@ -24,4 +23,26 @@ public class EmployeeApi {
     public List<Employee> getAllEmployee(){
         return employeeService.getAllEmployee();
     }
+
+    @DeleteMapping(path = "{id}")
+    public int deleteEmployee(@PathVariable("id") int id){
+        return employeeService.deleteEmployee(id);
+    }
+
+    @PostMapping
+    public int insertEmployee(@RequestBody Employee employee){
+        return employeeService.insertEmployee(employee);
+    }
+
+    @PutMapping(path = "{id}")
+    public int updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee){
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @GetMapping(path = "{id}")
+    public Employee getEmployeeByID(@PathVariable("id") int id){
+        return employeeService.getEmployeeByID(id);
+    }
+
 }
+
