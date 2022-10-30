52998a78f41774db85791110198a64cae127a27e
Gourav Kumar <67992259+Gourav-Kr@users.noreply.github.com>
2022-10-30 14:48:33 +0530

    Service Fix (#16)
    * Service nd Api
    
    * CustomerOrder Api
    
    * services corrected (#13)
    
    * Repo created
    
    * Services created
    
    * Update CustomerMysql.java
    
    * service corrected
    
    Co-authored-by: milan-s1 <milansomani.4oct@gmail.com>
    Co-authored-by: Deepanshu verma <73387559+iDeepverma@users.noreply.github.com>
    
    * Update project.sql
    
    * CustomerOrder Api (#14)
    
    * Service nd Api
    
    * CustomerOrder Api
    
    * Update project.sql
    
    Co-authored-by: Deepanshu verma <73387559+iDeepverma@users.noreply.github.com>
    
    * Revert "Merge branch 'main' of https://github.com/iDeepverma/DBMS_project"
    
    This reverts commit 525ee156d27f0c34521e307ab1ed7c521eda5147, reversing
    changes made to bee8fe29be7fda0f4334fbfe2bc3b664f2f3e71e.
    
    * Fix Bean Error
    
    * CustomerOrderService fix
    
    Co-authored-by: milan-s1 <94093613+milan-s1@users.noreply.github.com>
    Co-authored-by: milan-s1 <milansomani.4oct@gmail.com>
    Co-authored-by: Deepanshu verma <73387559+iDeepverma@users.noreply.github.com>

---
+++ src/main/java/com/shop/demo/service/SupplyOrderService.java
@@ -0,0 +1,53 @@
+package com.shop.demo.service;
+
+import com.shop.demo.dao.SupplyOrderDAO;
+import com.shop.demo.model.SupplyOrder;
+import com.shop.demo.model.SupplyOrderItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class SupplyOrderService {
+
+    private SupplyOrderDAO supplyOrderDAO;
+
+    @Autowired
+    SupplyOrderService(@Qualifier("supplyOrder_mysql_repo") SupplyOrderDAO supplyOrderDAO) {
+        this.supplyOrderDAO = supplyOrderDAO;
+    }
+
+    public int insertSupplyOrder(SupplyOrder supplyOrder) {
+
+        return supplyOrderDAO.insertSupplyOrder(supplyOrder);
+    }
+
+    public int deleteSupplyOrder(int id) {
+
+        return supplyOrderDAO.deleteSupplyOrder(id);
+    }
+
+
+    public int updateSupplyOrder(int id, SupplyOrder supplyOrder) {
+        return supplyOrderDAO.updateSupplyOrder(id, supplyOrder);
+    }
+
+    public SupplyOrder getSupplyOrderByID(int id) {
+        return supplyOrderDAO.getSupplyOrderByID(id);
+    }
+
+    public int updateDeliveryStatus(int id, String deliveryStatus) {
+        return supplyOrderDAO.updateDeliveryStatus(id, deliveryStatus);
+    }
+
+    public int updateDeliveryDate(int id, LocalDate date) {
+        return supplyOrderDAO.updateDeliveryDate(id, date);
+    }
+
+    public List<SupplyOrderItem> getSupplyItem(SupplyOrder supplyOrder) {
+        return supplyOrderDAO.getSupplyItem(supplyOrder);
+    }
+}
