4f3cefa12b29363df77cf78f0e77893c88da594f
Gourav Kumar <gouravcoc667@gmail.com>
2022-11-05 17:56:07 +0530

    Product Detail Page

--- src/main/java/com/shop/demo/appController/ErrorController.java
+++ src/main/java/com/shop/demo/appController/ErrorController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorController {
 
-    @GetMapping({"/error"})
-    public String homepage(){
-        return "error";
-    }
+     @GetMapping({"/error"})
+     public String homepage(){
+         return "error";
+     }
 }
